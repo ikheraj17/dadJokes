233aca315a63e60e2f0022cd06ea77516d23e812
Imran Kheraj <ikheraj17@gmail.com>
2020-06-24 15:45:17 -0500

    environment variables for passport oauth google strategy created, dotenv installed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const env = require('dotenv').config();
 app.use(express.static(__dirname + '/../public'));
 
 app.get('/', (req, res) => res.send('Express World!'));
