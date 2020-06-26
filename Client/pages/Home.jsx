08e97024990da9702ddcf5436321eb509f702ea1
Imran Kheraj <ikheraj17@gmail.com>
2020-06-26 15:11:24 -0500

    added a home page for logged out users, and a profile page for logged in users

---
+++ Client/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p>Welcome to the Wonderland of Dad Jokes!</p>
+            <button>Log in with Google to get started</button>
+        </div>
+    )
+};
+
+export default Home;
---
+++ Client/pages/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Profile = () => {
+    <div>
+        <p>
+            You have successfully logged in!
+            Enter your phone number below to start
+            the flow of daily dad jokes straight to your 
+            mobile device. 
+        </p>
+        <input type="text" plcaeholder="Enter a valid phone number"></input>
+        <button>Submit</button>
+    </div>
+};
+
+export default Profile;
