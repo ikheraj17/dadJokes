3183e0b9ce12cc4d0f5c69c1a76e1455235f50fc
Imran Kheraj <ikheraj17@gmail.com>
2020-06-26 14:51:18 -0500

    installed passport, passport google strategy, cors for server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const cors = require('cors');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const env = require('dotenv').config();
+const app = express();
+app.use(cors());
+app.use(passport.initialize());
+const port = 3000;
+
 app.use(express.static(__dirname + '/../public'));
 
 app.get('/', (req, res) => res.send('Express World!'));
