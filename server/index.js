5f45f6ce2fd8892fad713701eddef567335b9bd2
Imran Kheraj <ikheraj17@gmail.com>
2020-06-26 14:57:52 -0500

    server logic complete, may need to edit * route

--- server/index.js
+++ server/index.js
@@ -6,10 +6,41 @@ const env = require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(passport.initialize());
-const port = 3000;
+let user = {};
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+}, 
+    (accessToken, refreshToken, profile, cb) => {
+        console.log(JSON.stringify(profile));
+        user = {...profile};
+        return cb(null, profile);
+    }));
 
 app.use(express.static(__dirname + '/../public'));
 
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/'}), (req, res) => {
+    res.redirect('/profile');
+});
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/../public/index.html'));
+});
+
 app.get('/', (req, res) => res.send('Express World!'));
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
